0d9ba68f4cb6afc38af6651fc34f703bb2780cce
FAD\dimitar.navushtanov <dmnavushtanov@abv.bg>
2025-06-04 15:53:51 +0300

    clear everything

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello, world!")
+from smolagents import CodeAgent, DuckDuckGoSearchTool, InferenceClientModel, load_tool, tool
+import datetime
+import requests
+import pytz
+import yaml
+
+from Gradio_UI import GradioUI
+
+# Below is an example of a tool that does nothing. Amaze us with your creativity!
