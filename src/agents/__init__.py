26ca33185536856dd6f626a5907ab20df385e872
dmnavushtanov <dmnavushtanov@abv.bg>
2025-06-09 12:30:02 +0300

    Refine IssueInsightsAgent

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -4,10 +4,13 @@ import logging
 
 logger = logging.getLogger(__name__)
 logger.debug("agents package loaded")
-logger.info("ClassifierAgent and ApiValidatorAgent will be exported")
+logger.info(
+    "ClassifierAgent, ApiValidatorAgent and IssueInsightsAgent will be exported"
+)
 
 from .classifier import ClassifierAgent
 from .api_validator import ApiValidatorAgent
+from .issue_insights import IssueInsightsAgent
 
-__all__ = ["ClassifierAgent", "ApiValidatorAgent"]
+__all__ = ["ClassifierAgent", "ApiValidatorAgent", "IssueInsightsAgent"]
 
